c73b2042ea3291b33587ab15fa99902f77b48bac
Mohamed Ali <mo.ali12477@gmail.com>
2021-05-31 23:05:34 -0700

    Initial Commit

---
+++ InstaBug-Logging/InstaBug_Logging.h
@@ -0,0 +1,18 @@
+//
+//  InstaBug_Logging.h
+//  InstaBug-Logging
+//
+//  Created by Mohamed Ali on 31/05/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for InstaBug_Logging.
+FOUNDATION_EXPORT double InstaBug_LoggingVersionNumber;
+
+//! Project version string for InstaBug_Logging.
+FOUNDATION_EXPORT const unsigned char InstaBug_LoggingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InstaBug_Logging/PublicHeader.h>
+
+
